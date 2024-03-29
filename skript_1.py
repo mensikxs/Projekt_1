d5b6658f6845ab7c6f4bf1b0daedc2a3f84f82c4
mensikxs <mensikxs@gmail.com>
2024-03-29 14:04:42 +0100

    final

--- skript_1.py
+++ skript_1.py
@@ -6,35 +6,10 @@ email: simensikova@gmail.com
 discord: mensikxs@gmail.com
 
 """
+import string
+from task_template import TEXTS as text
 
-# list of texts for analysis
-TEXTS = ["""
-Situated about 10 miles west of Kemmerer,
-Fossil Butte is a ruggedly impressive
-topographic feature that rises sharply
-some 1000 feet above Twin Creek Valley
-to an elevation of more than 7500 feet
-above sea level. The butte is located just
-north of US 30N and the Union Pacific Railroad,
-which traverse the valley.""",
-         """At the base of Fossil Butte are the bright
-red, purple, yellow and gray beds of the Wasatch
-Formation. Eroded portions of these horizontal
-beds slope gradually upward from the valley floor
-and steepen abruptly. Overlying them and extending
-to the top of the butte are the much steeper
-buff-to-white beds of the Green River Formation,
-which are about 300 feet thick.""",
-         """The monument contains 8198 acres and protects
-a portion of the largest deposit of freshwater fish
-fossils in the world. The richest fossil fish deposits
-are found in multiple limestone layers, which lie some
-100 feet below the top of the butte. The fossils
-represent several varieties of perch, as well as
-other freshwater genera and herring similar to those
-in modern oceans. Other fish such as paddlefish,
-garpike and stingray are also present."""
-         ]
+separator = 40 * "-"
 
 # dictionary of registred users and their passwords:
 logins = {
@@ -44,140 +19,132 @@ logins = {
     "liz": "pass123"
 }
 
-separator = 50 * "-"
-
-# starting of program with welcome:
-print("Welcome!")
-print(separator)
-print("""
-+------+-------------+
-| user |   password  |
-+------+-------------+
-| bob  |     123     |
-| ann  |   pass123   |
-| mike | password123 |
-| liz  |   pass123   |
-+------+-------------+"""
-      )
-
 # entering username and password:
 name = input("Enter username: ")
 password = input("Enter password: ")
 
 # different situations when entering username and password:
-# 1 username and password are correct (registered)
+
+# 1) username and password are correct (registered user):
 if logins.get(name) == password:
+    
     print(separator)
 
     # welcome and give options:
+    available_text = len(text)
     print(
-        "Welcome to the app,", name,
-        "\nWe have 3 texts to be analyzed."
+        f"Welcome to the app, {name}!",
+        f"We have {available_text} texts to be analysed.",
+        sep= "\n"
     )
+    
     print(separator)
 
-    # choose number of text:
-    number = int(input(
-        "Enter a number btw. 1 and 3 to select: "
+    # choosing text:
+    number = input(
+        f"Enter a number btw. 1 and {available_text} to select: "
         )
-        )
-
+    
     print(separator)
 
     # different situations when choosing text:
-    # a) entered number is not in given range
-    if number not in range(1, len(TEXTS) + 1):
-        print("Wrong text number, terminating the program..")
+    # a) entered symbol is not a number
+    if not number.isnumeric():
+        print("Numbers only, terminating the program..")
         exit()
 
-    # b) entered symbol is not a number
-    elif not str(number).isnumeric():
-        print("Numbers only, terminating the program..")
+    # b) entered number is not in given range
+    elif int(number) not in range(1, len(text) + 1):
+        print("Wrong text number, terminating the program..")
         exit()
 
     # c) entered number is correct, program continues with analysis
     # of the chosen text:
     else:
-        # counting words in text
-        words = len(TEXTS[number-1].split())
+        number = int(number)
+
+        # a) counting words:
+        words = len(text[number-1].split())
         print("There are", words, "words in the selected text.")
 
-        # counting titlecase words
+        # b) counting titlecase words:
         title_case = len(
-            [title for title in TEXTS[number-1].split() if title.istitle()]
+            [title for title in text[number-1].split() if title.istitle()]
         )
         print("There are", title_case, "titlecase words.")
 
-        # counting uppercase words
+        # c) counting uppercase words:
         upper_case = len(
-            [upper for upper in TEXTS[number-1].split() if upper.isupper()]
+            [upper for upper in text[number-1].split()
+             if upper.isupper() and upper.isalpha()]
         )
         print("There are", upper_case, "uppercase words.")
 
-        # counting lowercase words
+        # d) counting lowercase words:
         lower_case = len(
-            [lower for lower in TEXTS[number-1].split() if lower.islower()]
+            [lower for lower in text[number-1].split() if lower.islower()]
         )
         print("There are", lower_case, "lowercase words.")
 
-        # counting numeric strings
-        numeric_string = len(
-            [numer for numer in TEXTS[number-1].split() if numer.isnumeric()]
-        )
-        print("There are", numeric_string, "numeric strings.")
+        # e) counting numeric strings + f) their sum:
+        numeric_string =[]
+        for num_str in text[number-1].split():
+            if num_str.isnumeric():
+                num_str = int(num_str)
+                numeric_string.append(num_str)
+        print("There are", len(numeric_string), "numeric strings.")
+        print(f"The sum of all the numbers is {sum(numeric_string)}.")
 
-        # turning numeric string into int and counting their sum
-        numbers = [
-            n for n in TEXTS[number-1].split() if n.isnumeric()
-        ]
-
-        for i in range(0, len(numbers)):
-            numbers[i] = int(numbers[i])
-
-        print("The sum of all the numbers is", str(sum(numbers)) + ".")
         print(separator)
 
-        # cutting text into words(without puncionation)
-        # and counting their length
-        lengths = ((
-            TEXTS[number-1].replace(",", "")
-        ).replace(".", "")
-        ).split()
+        # g) creating word length vs. frequency graph:
 
-        for j in range(0, len(lengths)):
-            lengths[j] = len(lengths[j])
+        # cleaning text from punctuation:
+        exclude = (string.punctuation)
+        clean_text = ""
+        for letter in text[number-1]:
+            if letter not in exclude:
+                clean_text += letter
 
-        count = []
-        for values in range(min(lengths), max(lengths) + 1):
-            count.append(lengths.count(values))
+        # spliting clean text into words and counting their lengths:
+        length = list(map(len, clean_text.split()))
 
+        # counting frequencies:
+        frequency = []
+        for values in range(min(length), max(length) + 1):
+            frequency.append(length.count(values))
+
+        # preparing graph components:
+        
+        # occurences represented by symbol "*":
         symbol = []
-        for m in range(len(count)):
-            symbol.append(count[m] * "*")
+        for j in range(len(frequency)):
+            symbol.append(frequency[j] * "*")
 
-        # chci cyklus, ktery kazde polozce v listu counts
-        # priradi poradove cislo:
-        seq = []
-        for cisla in range(1, len(count) + 1):
-            seq.append(cisla)
+        # lenghts of present words:
+        sequence = []
+        for seq_number in range(1, len(frequency) + 1):
+            sequence.append(str(seq_number))
 
+        # assembling graph lines:
         graph = []
         line = []
-        for row in range(len(count)):
+        for row in range(len(frequency)):
             line = [
-                (str(seq[row])).rjust(3)
-                + "| "
-                + str(symbol[row])
-                + " " * (max(count) - count[row])
-                + "|".rjust(3)
-                + str(count[row])
+                sequence[row].rjust(3),
+                "| ",
+                symbol[row],
+                " " * (max(frequency) - frequency[row]),
+                "|".rjust(3),
+                str(frequency[row])
             ]
-            graph.append(" ".join(line))
+            graph.append("".join(line))
 
+        # printing graph legend and graph:
         print(
-            "LEN|"
-            + "OCCURENCES".center(max(count) + 3)
-            + "|NR."
+            "LEN|",
+            "OCCURENCES".center(max(frequency) + 1),
+            "|NR.",
         )
         print(separator)
         print("\n".join(graph))
@@ -186,7 +153,7 @@ if logins.get(name) == password:
 
 
 # 2 Wrong username or password
-elif logins.get(name) is None:
+elif not logins.get(name):
     print("Unregistered user, terminating the program..")
     exit()
 
